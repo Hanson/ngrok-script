218ab7651f0ac64e90b1febb59feaa29698c80d5
HanSon <h@hanc.cc>
2017-04-09 23:04:35 +0800

    add mac script

--- ngrok.sh
+++ ngrok.sh
@@ -1,1 +1,9 @@
-./ngrok -config=./ngrok.cfg -subdomain  $1 80
+#!/usr/bin/env bash
+
+if [ "$(uname)" == "Darwin" ]
+then
+	./ngrok-mac -config=./ngrok.cfg -subdomain  $1 80
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]
+then
+	./ngrok -config=./ngrok.cfg -subdomain  $1 80
+fi
